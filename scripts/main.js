6bd24d4bb67c1db0e9b12b7c415717a5babf9789
Edwin Islas Aguirre <8241001@isd535.org>
2022-03-16 11:04:30 -0500

    js added, need more

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,9 @@
-// Put your JavaScript here
+const DETAIL_IMAGE_SELECTOR ='[data-image-role="target"]';
+const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+
+
+function setDetails() {
+    'use strict'
+    let detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+    detailImage.setAttribute('src' , 'img/img/projecthopelogo2.webp');
+}
