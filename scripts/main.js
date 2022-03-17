e5377ec4d45f7d4d8585e634d992f9c72e5d68a3
Edwin Islas Aguirre <8241001@isd535.org>
2022-03-16 19:14:47 -0500

    minor changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,13 @@
 const DETAIL_IMAGE_SELECTOR ='[data-image-role="target"]';
+const DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
 
 
-function setDetails(imageUrl) {
+function setDetails(imageUrl, titleText) {
     'use strict'
     let detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
-    detailImage.setAttribute('src' , 'imageUrl');
+    detailImage.setAttribute('src' , imageUrl);
+
+    let detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+    detailImage.textContext = titleText;
 }
