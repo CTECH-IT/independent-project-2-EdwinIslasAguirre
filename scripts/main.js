557a6d9f5c336906fe55e8641ab14938ab349cbc
Edwin Islas Aguirre <8241001@isd535.org>
2022-03-16 22:06:23 -0500

    major changes and major questions

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,6 @@ const DETAIL_IMAGE_SELECTOR ='[data-image-role="target"]';
 const DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
 
-
 function setDetails(imageUrl, titleText) {
     'use strict';
     let detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
@@ -26,3 +25,27 @@ function setDetailsFromThumb(thumbnail) {
     'use strict';
     setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
 }
+
+function addThumbClickHandler(thumb) {
+addEventListener('click', function(event) {
+    event.preventDefault();
+    setDetailsFromThumb(thumb);
+});
+}
+
+function getThumbnailsArray() {
+    'use strict';
+    let thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+    let thumbnailArray = [].slice.call(thumbnails); // convert NodeList to an Array
+    return thumbnailArray;
+}
+
+function initializeEvents() {
+    'use strict';
+    let thumbnails = getThumbnailsArray();
+    thumbnails.forEach(addThumbClickHandler);
+}
+
+// run all the functions to link to the callback
+// that will update the main detail image with the thumbnails image and title
+initializeEvents();
